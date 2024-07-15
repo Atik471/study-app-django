50c65ad4ee664b51183c38ac893997bd015d943a
Atik471 <108218772+Atik471@users.noreply.github.com>
2024-07-15 11:42:29 +0600

    added models

--- base/views.py
+++ base/views.py
@@ -1,21 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 
-
-rooms = [
-    {'id':1, 'name': 'Lets learn django haha'},
-    {'id':2, 'name': 'Lets learn django woohoo'},
-    {'id':3, 'name': 'Lets learn django yeahhh'},
-]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms' : rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
